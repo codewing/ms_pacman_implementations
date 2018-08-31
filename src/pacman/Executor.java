c9d4323cef471196a5c1e98071f16434b7b9d0fc
Winfried Baumann <codewing@web.de>
2018-08-31 10:30:43 +0200

    updated the executor to use mypacman

--- src/pacman/Executor.java
+++ src/pacman/Executor.java
@@ -22,6 +22,7 @@ import pacman.controllers.examples.RandomNonRevPacMan;
 import pacman.controllers.examples.RandomPacMan;
 import pacman.controllers.examples.StarterGhosts;
 import pacman.controllers.examples.StarterPacMan;
+import pacman.entries.pacman.MyPacMan;
 import pacman.game.Game;
 import pacman.game.GameView;
 
@@ -58,13 +59,13 @@ public class Executor
 		exec.runGame(new RandomPacMan(),new RandomGhosts(),visual,delay);
   		 */
 		
-		///*
+
 		//run the game in asynchronous mode.
 		boolean visual=true;
-//		exec.runGameTimed(new NearestPillPacMan(),new AggressiveGhosts(),visual);
-		exec.runGameTimed(new StarterPacMan(),new StarterGhosts(),visual);
-//		exec.runGameTimed(new HumanController(new KeyBoardInput()),new StarterGhosts(),visual);	
-		//*/
+		//exec.runGameTimed(new NearestPillPacMan(),new AggressiveGhosts(),visual);
+		exec.runGameTimed(new MyPacMan(),new StarterGhosts(), visual);
+		//exec.runGameTimed(new HumanController(new KeyBoardInput()),new StarterGhosts(),visual);
+
 		
 		/*
 		//run the game in asynchronous mode but advance as soon as both controllers are ready  - this is the mode of the competition.
